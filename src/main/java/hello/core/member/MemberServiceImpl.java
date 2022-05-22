5e690a874fd0dedb5d913187b48fbfc85d48d134
boram <5018b@naver.com>
2022-05-22 17:44:19 +0900

    2. 객체 지향 원리 적용 > 관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,12 @@ package hello.core.member;
 //인터페이스가 한개인 경우 Impl로 구현 클래스명 작성한다.
 public class MemberServiceImpl implements MemberService{
     //아래 코드는 추상화와 구체화 모두를 의존하고 있어서 좋지 못하다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,31 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    //문제점 : 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 고쳐야 한다.
+    //DIP 위반 : 추상뿐 아니라 구체 클래스에도 의존을 하고 있음
+    /**
+     * OCP 위반 : 지금 코드는 기능을 확장하면 클라이언트 코드에 영향을 준다.
+     * FixDiscountPolicy 를 RateDiscountPolicy로 변경하는 순간 OrderServiceImpl 의 소스 코드도 함께 변경해야 함
+     */
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //아래처럼 한다면?
+    private final DiscountPolicy discountPolicy; //NPE 발생
+    //위를 해결하려면? 누군가가 클라이언트인 OrderServiceImpl 에 DiscountPolicy 의 구현 객체를 대신 생성하고 주입해야한다 -> DI 임
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
